fb2e6cb59717c54504fe98879dbf2944499616e7
Jaidon Clinton <jaidcoding@gmail.com>
2025-01-11 14:09:52 -0600

    Upgrade React and ReactDOM to version 18.3.1 and refactor main.jsx to use createRoot for rendering

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
-ReactDOM.render(
+// Get the root element
+const container = document.getElementById('root');
+// Create a root
+const root = createRoot(container);
+
+// Initial render
+root.render(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
