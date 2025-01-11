40e426be1e9bbd978b9654ac41ed3dc4c02156d5
validai <djcoast239@gmail.com>
2025-01-10 19:22:28 -0500

    Commit local changes before pulling

--- src/config/viteconfig.js
+++ src/config/viteconfig.js
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: './src', // Adjust the root if needed
+  root: './frontend', // Set to the directory containing index.html
   build: {
-    outDir: '../dist', // Output directory for your bundled files
+    outDir: './dist', // Output directory for the build
   },
   server: {
     proxy: {
-      '/api': 'http://localhost:3000', // Proxy API requests to your Express back end
+      '/api': 'http://localhost:3000', // Proxy API requests to the backend
     },
   },
 });
